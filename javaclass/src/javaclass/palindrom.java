b5111de0f4de028458b08de6920b38af5afedbd9
Shreesh1123 <srishk119@gmail.com>
2020-01-23 13:12:44 +0530

    dtgdfduy

---
+++ javaclass/src/javaclass/palindrom.java
@@ -0,0 +1,21 @@
+package javaclass;
+
+public class palindrom {
+	public static void main(String[]args){
+	int num = 121;
+	int rev = 0;
+	int temp = num;
+	while(num>0)
+	{
+		int rem = num%10;
+		rev = (rev*10)+rem;
+		num=num/10;
+	}
+	if(temp==rev)
+		System.out.println("pass");
+	else
+		System.out.println("fail");
+	}
+
+}
+ 
